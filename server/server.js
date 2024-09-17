3e7e229fa6e119728d40c1a4018705c05f29b449
Dorisa Shehi <doris.shehi2@gmail.com>
2024-09-17 13:24:15 +0200

    Code tu run the server added

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+
+const PORT = process.env.port || 3001; //read port fron env file else 3001 by default
+app.use("/public", express.static("./public")); //serve frontand files from client/public
+app.use("/public", express.static("./public/scripts")); //serve script files from client/public/scripts
+
+app.get("/", (req, res) => {
+  //route when we type / at browser
+  res
+    .status(200)
+    .send(
+      '<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>'
+    );
+});
+
+app.listen(PORT, () => {
+  //Make app to listen to the PORT constant
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
